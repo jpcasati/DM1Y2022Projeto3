f5eb6ee9a03a67be68052deb8a5ee2124ac02ed0
JPCASATI <jpcasati@usp.br>
2022-09-28 20:53:16 -0300

    utilizando binding na tela principal e deletando itens da lista com alert dialog

--- app/src/main/java/br/edu/mouralacerda/dm1y2022projeto3/view/MainActivity.kt
+++ app/src/main/java/br/edu/mouralacerda/dm1y2022projeto3/view/MainActivity.kt
@@ -5,24 +5,59 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import br.edu.mouralacerda.dm1y2022projeto3.R
+import br.edu.mouralacerda.dm1y2022projeto3.databinding.ActivityMainBinding
 import br.edu.mouralacerda.dm1y2022projeto3.model.Produto
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
+    private val b by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     val listaDeProdutos = ArrayList<Produto>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(b.root)
 
-        val botaoAdicionar = findViewById<FloatingActionButton>(R.id.btnNovoProduto)
-        botaoAdicionar.setOnClickListener {
+        b.btnNovoProduto.setOnClickListener {
 
             val intencao = Intent(this, CadastroProduto::class.java)
             startActivityForResult(intencao, 1)
+        }
+
+        b.btnNovoProduto.setOnLongClickListener {
+            Toast.makeText(
+                this,
+                "Clique rápido para adicionar",
+                Toast.LENGTH_SHORT
+            ).show()
+            true
+        }
+
+        b.lstProdutos.setOnItemLongClickListener { adapterView, view, i, l ->
+
+            val builder = AlertDialog.Builder(this)
+            builder
+                .setTitle("Apagar Produto")
+                .setMessage("Deseja realmente apagar o produto selecionado?")
+                .setPositiveButton("Sim") { dialog, which ->
+                    listaDeProdutos.removeAt(i)
+                    atualizarLista()
+                    Toast.makeText(this, "Produto removido com sucesso!",
+                        Toast.LENGTH_LONG).show()
+            }
+            .setNegativeButton("Não") { dialog, which ->
+                Toast.makeText(this, "Produto não removido!",
+                    Toast.LENGTH_LONG).show()
+            }
+            .show()
+            true
         }
 
     }
@@ -46,7 +81,7 @@ class MainActivity : AppCompatActivity() {
             listaDeProdutos
         )
 
-        findViewById<ListView>(R.id.lstProdutos).adapter = adp
+        b.lstProdutos.adapter = adp
     }
 
 }
